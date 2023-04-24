6a1a8ccf5e153cedcec50f3a084f1af971cd76ee
iamabhishek <abhishekkumarsingh20012000@gmail.com>
2023-04-25 00:24:38 +0530

    'check'

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,83 +1,104 @@
 import Image from "next/image";
 import React from "react";
-
-const skills = [
-  {
-    name: "HTML",
-    source: "/../public/assets/projects/html.png",
-  },
-  {
-    name: "CSS",
-    source: "/../public/assets/projects/css.png",
-  },
-  {
-    name: "JavaScript",
-    source: "/../public/assets/projects/javascript.png",
-  },
-
-  {
-    name: "Python",
-    source: "/../public/assets/projects/python.png",
-  },
-
-  {
-    name: "Tailwind",
-    source: "/../public/assets/projects/tailwind.png",
-  },
-  {
-    name: "React",
-    source: "/../public/assets/projects/react.png",
-  },
-  {
-    name: "Next",
-    source: "/../public/assets/projects/next.png",
-  },
-  {
-    name: "Node",
-    source: "/../public/assets/projects/node.png",
-  },
-  {
-    name: "Express",
-    source: "/../public/assets/projects/express.png",
-  },
-  {
-    name: "MongoDB",
-    source: "/../public/assets/projects/mongo.png",
-  },
-  {
-    name: "Github",
-    source: "/../public/assets/projects/github1.png",
-  },
-  {
-    name: "Firebase",
-    source: "/../public/assets/projects/firebase.png",
-  },
-];
+import Html from "../public/assets/skills/html.png";
+import Css from "../public/assets/skills/css.png";
+import Javascript from "../public/assets/skills/javascript.png";
+import ReactImg from "../public/assets/skills/react.png";
+import Tailwind from "../public/assets/skills/tailwind.png";
+import Github from "../public/assets/skills/github1.png";
+import Firebase from "../public/assets/skills/firebase.png";
+import NextJS from "../public/assets/skills/nextjs.png";
+import AWS from "../public/assets/skills/aws.png";
 
 const Skills = () => {
   return (
     <div id="skills" className="w-full lg:h-screen p-2">
       <div className="max-w-[1240px] mx-auto flex flex-col justify-center h-full">
-        <p className="uppercase text-xl tracking-widest text-[#5651e5]">
+        <p className="text-xl tracking-widest uppercase text-[#5651e5]">
           Skills
         </p>
         <h2 className="py-4">What I Can Do</h2>
-        <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
-          {skills.map(({ name, source }, index) => (
-            <div
-              key={index}
-              className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300"
-            >
-              <div className="grid grid-cols-2 gap-4 justify-center items-center">
-                <div className="m-auto">
-                  <Image src={source} alt="/" width={64} height={64} />
-                </div>
-                <div className="flex flex-col items-center justify-center">
-                  <h3>{name}</h3>
-                </div>
+        <div className="grid grid-cols-2 lg:grid-cols-4 gap-8">
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image src={Html} width="64px" height="64px" alt="/" />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>HTML</h3>
               </div>
             </div>
-          ))}
+          </div>
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image src={Css} width="64px" height="64px" alt="/" />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>CSS</h3>
+              </div>
+            </div>
+          </div>
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image src={Javascript} width="64px" height="64px" alt="/" />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>JavaScript</h3>
+              </div>
+            </div>
+          </div>
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image src={ReactImg} width="64px" height="64px" alt="/" />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>React</h3>
+              </div>
+            </div>
+          </div>
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image src={Tailwind} width="64px" height="64px" alt="/" />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>Tailwind</h3>
+              </div>
+            </div>
+          </div>
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image src={Firebase} width="64px" height="64px" alt="/" />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>Firebase</h3>
+              </div>
+            </div>
+          </div>
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image src={Github} width="64px" height="64px" alt="/" />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>Github</h3>
+              </div>
+            </div>
+          </div>
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image src={NextJS} width="64px" height="64px" alt="/" />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>Next</h3>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -85,3 +106,91 @@ const Skills = () => {
 };
 
 export default Skills;
+
+// import Image from "next/image";
+// import React from "react";
+
+// const skills = [
+//   {
+//     name: "HTML",
+//     source: "../public/assets/projects/html.png",
+//   },
+//   {
+//     name: "CSS",
+//     source: "/../public/assets/projects/css.png",
+//   },
+//   {
+//     name: "JavaScript",
+//     source: "/../public/assets/projects/javascript.png",
+//   },
+
+//   {
+//     name: "Python",
+//     source: "/../public/assets/projects/python.png",
+//   },
+
+//   {
+//     name: "Tailwind",
+//     source: "/../public/assets/projects/tailwind.png",
+//   },
+//   {
+//     name: "React",
+//     source: "/../public/assets/projects/react.png",
+//   },
+//   {
+//     name: "Next",
+//     source: "/../public/assets/projects/next.png",
+//   },
+//   {
+//     name: "Node",
+//     source: "/../public/assets/projects/node.png",
+//   },
+//   {
+//     name: "Express",
+//     source: "/../public/assets/projects/express.png",
+//   },
+//   {
+//     name: "MongoDB",
+//     source: "/../public/assets/projects/mongo.png",
+//   },
+//   {
+//     name: "Github",
+//     source: "/../public/assets/projects/github1.png",
+//   },
+//   {
+//     name: "Firebase",
+//     source: "/../public/assets/projects/firebase.png",
+//   },
+// ];
+
+// const Skills = () => {
+//   return (
+//     <div id="skills" className="w-full lg:h-screen p-2">
+//       <div className="max-w-[1240px] mx-auto flex flex-col justify-center h-full">
+//         <p className="uppercase text-xl tracking-widest text-[#5651e5]">
+//           Skills
+//         </p>
+//         <h2 className="py-4">What I Can Do</h2>
+//         <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
+//           {skills.map(({ name, source }, index) => (
+//             <div
+//               key={index}
+//               className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300"
+//             >
+//               <div className="grid grid-cols-2 gap-4 justify-center items-center">
+//                 <div className="m-auto">
+//                   <img src={source} alt="/" width={64} height={64} />
+//                 </div>
+//                 <div className="flex flex-col items-center justify-center">
+//                   <h3>{name}</h3>
+//                 </div>
+//               </div>
+//             </div>
+//           ))}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Skills;
